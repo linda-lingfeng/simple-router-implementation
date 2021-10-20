5eeeef47f7a729576fdcb19b038cf096f188a149
Linda Lo <adnil3910@gmail.com>
2021-10-20 13:06:28 -0700

    Wrote functions to create etherframe and arp

--- router/sr_router.c
+++ router/sr_router.c
@@ -87,4 +87,86 @@ void sr_handlepacket(struct sr_instance* sr,
 
 }/* end sr_handlepacket */
 
+/*---------------------------------------------------------------------
+ * Method: sr_create_etherpacket (unsigned int load_len, uint8_t* load,
+ * uint8_t* dest_ether_ddr, uint8_t* source_ether_addr, uint16_t ether_type)
+ * Output: uint8_t* (pointer to allocated frame)
+ * Scope:  Global
+ *
+ * This method allocates space for an ethernet frame given a particular
+ * load, load_len, source and destination MAC addresses and ethernet type.
+ * The frame is return as a pointer to the buffer in network byte order.
+ *---------------------------------------------------------------------*/
 
+uint8_t* sr_create_etherframe (unsigned int load_len,
+                                uint8_t* load,
+                                uint8_t* dest_ether_addr,
+                                uint8_t* source_ether_addr,
+                                uint16_t ether_type)
+{
+  sr_ethernet_hdr_t* frame = 0;
+
+  /*Allocate space for the ethernet frame*/
+  frame = (sr_ethernet_hdr_t*)malloc(sizeof(sr_ethernet_hdr_t) + load_len);
+
+  /* fill in the required fields*/
+  memcpy(frame->ether_dhost, dest_ether_addr, ETHER_ADDR_LEN);
+  memcpy(frame->ether_shost, source_ether_addr, ETHER_ADDR_LEN);
+  frame->ether_type=htons(ether_type);
+
+  /*Add load and return pointer*/
+  frame = (uint8_t*) frame;
+  memcpy(frame + sizeof(sr_ethernet_hdr_t), load, load_len);
+
+  return frame;
+}
+
+/*---------------------------------------------------------------------
+ * Method: sr_arppacket (unsigned int* len, enum sr_arp_opcode arp_type,
+ * unsigned char* source_ether_addr, uint32_t source_protocol_addr,
+ * unsigned char* dest_ether_addr, uint32_t dest_protocol_addr)
+ * Output: sr_arp_packet_t* (Pointer to allocated arp packet)
+ * Scope:  Local
+ *
+ * This method allocates space for an arp packet given the arp type,
+ * source hardware and protocol addresses and destination hardware and
+ * protocol addresses. It returns a pointer to the packet with all fields
+ * in network byte order.
+ * 
+ * It also fills in the length of the packet in bytes using "len".
+ *---------------------------------------------------------------------*/
+uint8_t* sr_create_arppacket(unsigned int* len,
+                             unsigned short arp_type,
+                             unsigned char* source_ether_addr,
+                             uint32_t source_protocol_addr,
+                             unsigned char* dest_ether_addr,
+                             uint32_t dest_protocol_addr)
+{
+  
+  sr_arp_packet_t* arp_packet = 0;
+  arp_packet = (sr_arp_packet_t*)malloc(sizeof(sr_arp_packet_t));
+  
+  if (arp_packet) {
+    /* Set hardware, protocol type and length */
+    arp_packet->ar_hrd = htons(arp_hrd_ethernet);
+    arp_packet->ar_pro = htons(arp_pro_ip);
+    arp_packet->ar_hln = ETHER_ADDR_LEN;
+    arp_packet->ar_pln = IP_ADDR_LEN;
+
+    /* Set ARP op code*/
+    arp_packet-> ar_op = htons(arp_type);
+
+    /*Set hardware, protocol source and destination
+     *Note assumes ethernet addresses are in network byte order*/
+    memcpy(arp_packet->ar_sha, source_ether_addr, ETHER_ADDR_LEN);
+    arp_packet->ar_sip = htonl(source_protocol_addr);
+
+    memcpy(arp_packet->ar_tha, dest_ether_addr, ETHER_ADDR_LEN);
+    arp_packet->ar_tip = htonl(dest_protocol_addr);
+
+    /* Set length of packet*/
+    *len=sizeof(sr_arp_packet_t);
+  }
+  
+  return (uint8_t*)arp_packet;
+}; /* end sr_create_arppacket */
